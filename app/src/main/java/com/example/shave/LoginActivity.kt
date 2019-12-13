2125a905562b8e275fb6ee8a9e3075810f7ca06a
mcalda <murat.calda@gmail.com>
2019-12-13 21:24:05 +0300

    hey

---
+++ app/src/main/java/com/example/shave/LoginActivity.kt
@@ -0,0 +1,56 @@
+package com.example.shave
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity :AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        button_login.setOnClickListener {
+            val email = email_login.text.toString()
+            val password = password_login.text.toString()
+            if(email.isEmpty()) {
+                Toast.makeText(this,"Please enter an e-mail address",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            else if(password.isEmpty()) {
+                Toast.makeText(this,"Please enter password",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+
+            }
+
+
+            Log.d("LoginActivity","Attempt to login with email and password")
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("LoginActivity", "signInWithEmail:success")
+                        val user = auth.currentUser
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("LoginActivity", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        //updateUI(null)
+                    }
+
+                    // ...
+                }
+        }
+
+        to_register_text.setOnClickListener {
+            finish()
+        }
+
+    }
+}
